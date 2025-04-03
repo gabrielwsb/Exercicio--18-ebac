19cb80a84d9667497137082f223449c4ecb0ce4c
gabrielwsb <gabrielwsb@me.com>
2025-04-03 12:33:55 +0000

    arquivo gasolina.csv criado

---
+++ gasolina.py
@@ -0,0 +1,17 @@
+
+ # código de geração do gráfico  
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Gerando o DataFrame
+gasolina_df = pd.read_csv('gasolina.csv', sep=',')
+gasolina_df
+
+# Gerando o gráfico
+with sns.axes_style('whitegrid'):
+  grafico = sns.lineplot(data=gasolina_df, x='dia', y='venda')
+  grafico.set(title='Variação do preço da gasolina', xlabel='Dia', ylabel='Preço');
+
+# Salvando o gráfico
+plt.savefig('gasolina.png', dpi=300, bbox_inches='tight')
